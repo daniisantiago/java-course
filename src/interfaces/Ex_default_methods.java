969b4672b92fde764b08ba811cae6f395f8bcba5
Daniella Santiago <danii.santiago@gmail.com>
2020-01-14 23:24:39 -0300

    trabalhando com metodos default na interface

---
+++ src/interfaces/Ex_default_methods.java
@@ -0,0 +1,33 @@
+package interfaces;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.services.BrazilInterestService;
+import model.services.InterestService;
+import model.services.UsaInterestService;
+
+public class Ex_default_methods {
+
+	//usando interfaces com default methods
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Amount: ");
+		double amount = sc.nextDouble();
+		System.out.print("Months: ");
+		int months = sc.nextInt();
+		
+		//InterestService is = new BrazilInterestService(2.0);
+		InterestService is = new UsaInterestService(1.0);
+		double payment = is.payment(amount, months);
+		
+		System.out.println("Payment after " + months + " months:");
+		System.out.println(String.format("%.2f", payment));
+		
+		sc.close();
+
+	}
+
+}
---
+++ src/model/services/UsaInterestService.java
@@ -0,0 +1,18 @@
+package model.services;
+
+public class UsaInterestService implements InterestService{
+	
+	private double interestRate;
+
+	public UsaInterestService(double interestRate) {
+		this.interestRate = interestRate;
+	}
+
+	@Override
+	public double getInterestRate() {
+		return interestRate;
+	}
+
+	
+	
+}
