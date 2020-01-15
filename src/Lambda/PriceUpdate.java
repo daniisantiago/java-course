a494bd50354c493c951beb7e99812ee1e3e8af3a
Daniella Santiago <danii.santiago@gmail.com>
2020-01-15 20:32:43 -0300

    usando a interface consumer na expressao lambda

--- src/Lambda/PriceUpdate.java
+++ src/Lambda/PriceUpdate.java
@@ -2,26 +2,27 @@ package Lambda;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import model.entities.Product;
 
-public class FuncaoAnonima {
+public class PriceUpdate {
 
 	public static void main(String[] args) {
 		
+		Locale.setDefault(Locale.US);
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("TV", 900.0));
 		list.add(new Product("Notebook", 1200.0));
 		list.add(new Product("Tablet", 400.0));
-	
-		//definindo que é pra ser odernado pelo nome		
-		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		list.add(new Product("Mouse", 50.0));
 		
-		for(Product p : list) {
-			System.out.println(p);
-		}
-	
+		//percorre a lista e adiciona 10% ao valor
+		list.forEach(p -> p.setPrice(p.getPrice() * 1.1));
+		
+		//percorre a lista e imprime os valores
+		list.forEach(System.out::println);
 
 	}
 
