ba589492670bfba83d794d0e3877551f36a440c2
Daniella Santiago <danii.santiago@gmail.com>
2020-01-15 19:18:46 -0300

    usando o Comparator como expressão lambda

---
+++ src/Lambda/FuncaoAnonima.java
@@ -0,0 +1,29 @@
+package Lambda;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import model.entities.Product;
+
+public class FuncaoAnonima {
+
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		
+		list.add(new Product("TV", 900.0));
+		list.add(new Product("Notebook", 1200.0));
+		list.add(new Product("Tablet", 400.0));
+	
+		//definindo que é pra ser odernado pelo nome		
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+		
+		for(Product p : list) {
+			System.out.println(p);
+		}
+	
+
+	}
+
+}
