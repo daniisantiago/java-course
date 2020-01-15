c2bf4510fe44c12e7e4720d682c3f7b9965004c8
Daniella Santiago <danii.santiago@gmail.com>
2020-01-15 17:44:09 -0300

    usando o TreeSet para comparar elementos

--- src/set/Comparando.java
+++ src/set/Comparando.java
@@ -2,6 +2,7 @@ package set;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 import model.entities.Product;
 
@@ -25,7 +26,20 @@ public class Comparando {
 		 * Para que a comparação de certo, seria necessario implementar o hashCode
 		 * e o equals na classe que está sendo usada.
 		 * */
+		
+		Set<Product> set1 = new TreeSet<>();
+		
+		set1.add(new Product("TV", 900.0));
+		set1.add(new Product("Notebook", 1200.0));
+		set1.add(new Product("Tablet", 400.0));
+				
+		for(Product p : set1) {
+			System.out.println(p);
+		}
 
+		// Para comparar usando o TreeSet é necessario que a classe implemente o comparable
+		
+		// no caso da classe Product, coloquei o compareTo para comparar atraves dos preços de cada produto 
 	}
 
 }
